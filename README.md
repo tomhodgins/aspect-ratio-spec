ef1009bdb72d1d2d95b52d6d4f739a4526b39cbf
Tom Hodgins <tomhodgins@gmail.com>
2016-12-06 13:29:57 -0500

    Add demo page, reference implementation plugin, syntax example, and create the spec document

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # aspect-ratio-spec
-A spec for an aspect-ratio property in CSS
+
+A spec for an Aspect Ratio property in CSS
+
+## What is this about?
+
+This spec aims to define an `aspect-ratio` property to provide a way CSS authors can inform the browser of a desired aspect ratio for an element unless a more specific `width` and `height` are defined.
+
+## Who am I?
+
+I'm an independent web developer, not affiliated with any major company. The research & development contained in this spec is my own.
+
+## How you can help
+
+At this stage the work on this spec is focused on specifying the behaviour described in this [Github thread]() as well as the plugin working at [staticresource.com/aspect-ratio.html](http://staticresource.com/aspect-ratio.html).
+
+For now, the most productive way to provide input would be by contributing to the conversation in the [Gitter chat room for the EQCSS project](https://gitter.im/eqcss/eqcss).
---
+++ plugin/aspect-ratio.js
@@ -0,0 +1,114 @@
+/*
+
+# Rational
+An aspect ratio polyfill for CSS
+
+## Usage
+
+This polyfill supports a custom `aspect-ratio` property which is not currently in CSS. It works using the `-eq` prefix, using the following syntax:
+
+    selector {
+      -eq-aspect-ratio: width / height;
+    }
+
+So for example, to give a 16/9 aspect ratio to a div:
+
+    div {
+      -eq-aspect-ratio: 16/9;
+    }
+
+You can also supply a `width` ~or~ `height`, but if at any time you have supplied both a `width` ~and~ `height` they take precedence over any `aspect-ratio` setting you may have defined.
+
+written by @innovati
+
+*/
+
+var rules = []
+
+function load(){
+  // Load <style> tags
+  var tag = document.querySelectorAll('style')
+  for (var i=0;i<tag.length;i++){
+    parse(tag[i].innerHTML)
+  }
+  // load <link> tags
+  var tag = document.querySelectorAll('link')
+  for (var i=0;i<tag.length;i++){
+    if (tag[i].getAttribute('rel') === 'stylesheet'){
+      if (tag[i].href){
+        (function(){
+          var xhr = new XMLHttpRequest
+          xhr.open('GET', tag[i].href, true)
+          xhr.send(null)
+          xhr.onload = function(){
+            parse(xhr.responseText)
+          }
+        })()
+      }
+    }
+  }
+}
+
+function parse(text){
+  var css = text || ''
+  css.replace(/\n/g,'').replace(/\}/g,'}\n').replace(/\s*{/g,'{').replace(/^\s*/gm,'').replace(/^(.*?)\s?{.*-eq-aspect-ratio:\s*(\d*\.?\d+)\/(\d*\.?\d+)/gm,function(match,$1,$2,$3){
+    var rule = [$1,$2,$3]
+    rules.push(rule)
+  })
+  apply()
+}
+
+function apply(){
+  for (var i=0;i<rules.length;i++){
+    var tag = document.querySelectorAll(rules[i][0])
+    for (var j=0;j<tag.length;j++){
+      var natWidth = getRule(tag[j],'width') || false,
+          natHeight = getRule(tag[j],'height') || false,
+          currWidth = tag[j].offsetWidth,
+          currHeight = tag[j].offsetHeight,
+          rWidth = rules[i][1],
+          rHeight = rules[i][2]
+      if (!natWidth && !natHeight){
+        tag[j].style.height = currWidth / (rWidth/rHeight) + 'px'
+      }
+      if (natWidth && !natHeight){
+        tag[j].style.height = currWidth / (rWidth/rHeight) + 'px'
+      }
+      if (!natWidth && natHeight){
+        tag[j].style.width = currHeight * (rWidth/rHeight) + 'px'
+      }
+    }
+  }
+}
+
+function getRule(tag,query){
+  var sheets = document.styleSheets,
+      matchedRules = []
+  if (!tag.matches) {
+    tag.matches =
+      tag.matchesSelector
+      || tag.mozMatchesSelector
+      || tag.msMatchesSelector
+      || tag.oMatchesSelector
+      || webkitMatchesSelector
+      || function(s){
+          var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+              m = matches.length
+           while (--m >= 0 && matches.item(m) !== this){}
+           return m > -1
+         }
+  }
+  for (var i=0; i<sheets.length; i++){
+    var css = sheets[i].rules || sheets[i].cssRules || ''
+    for (var j=0; j<css.length; j++){
+      if (tag.matches(css[j].selectorText)){
+        if (css[j].cssText.indexOf(query) !== -1) {
+          return true
+        }
+      }
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded',load)
+window.addEventListener('resize',apply)
